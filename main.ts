f36b8546b2bb08e8d1266b6614f85b1df4b6b7c8
Grzegorz Dominiczak <grzegorz@codehard.pl>
2022-07-05 00:33:32 +0200

    Add basic functionality for calculating time diffs

--- main.ts
+++ main.ts
@@ -1,137 +1,69 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
+import { differenceInMinutes } from "date-fns";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class TimeDiffPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		const timeRegex = /\d{2}:\d{2} - \d{2}:\d{2}/g;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		this.registerMarkdownCodeBlockProcessor(
+			"timediff",
+			(source, el, ctx) => {
+				let totalSumInMinutes = 0;
+				const rows = source.split("\n").filter((row) => row.length > 0);
+				for (const row of rows) {
+					const match = row.match(timeRegex);
+					if (!match) {
+						const div = el.createEl("div");
+						div.createEl("span", {
+							text: `${row}`,
+						});
+						continue;
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					const timeElements = match[0].trim().split(" - ");
+					const [left, right] = timeElements.map((timeElement) => {
+						const [hours, minutes] = timeElement.split(":");
+						return new Date().setHours(
+							Number(hours),
+							Number(minutes)
+						);
+					});
+					const totalDiffInMinutes = differenceInMinutes(right, left);
+					totalSumInMinutes += totalDiffInMinutes;
+					const { readableDiff } =
+						calculateTimeDiffs(totalDiffInMinutes);
+					const div = el.createEl("div");
+					div.createEl("span", {
+						text: `${row}`,
+					});
+					const diffSpan = div.createEl("span", {
+						text: `\t${readableDiff}`,
+					});
+					diffSpan.style.color = "var(--interactive-accent)";
 				}
+				const div = el.createEl("div");
+				div.createEl("span", {
+					text: `Total: `,
+				});
+				const diffSpan = div.createEl("span", {
+					text: `${
+						calculateTimeDiffs(totalSumInMinutes).readableDiff
+					}`,
+				});
+				diffSpan.style.color = "var(--interactive-accent)";
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		);
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	onunload() {}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function calculateTimeDiffs(totalDiffInMinutes: number): {
+	diffInHours: number;
+	diffInMinutes: number;
+	readableDiff: string;
+} {
+	const diffInHours = Math.floor(totalDiffInMinutes / 60);
+	const diffInMinutes = totalDiffInMinutes % 60;
+	const readableDiff = `${diffInHours}h${diffInMinutes}min`;
+	return { diffInHours, diffInMinutes, readableDiff };
 }
